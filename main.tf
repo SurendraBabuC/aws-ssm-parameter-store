3a87ec9021e2e3387eb9e41061e1fec75148a363
C SURENDRA BABU <surendrababu.c01@gmail.com>
2023-05-27 19:51:39 +0530

    Ansible

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "aws_ssm_parameter" "parameters" {
   count = length(var.parameters)
   name   = var.parameters[count.index].name
   value  = var.parameters[count.index].value
-  type   = "SecureString"
+  type   = "String"
   key_id = "4de4f1a6-10ef-4edb-9e58-e1199f52dd52"
 }
